5d70c0c74e0d1aca0e3ee6fdb4b14fffe7e461f4
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-05-04 12:40:14 +0530

    Navbar and Herosection

---
+++ src/app/components/Herosection.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import { Spotlight } from './ui/Spotlight';
+import { Button } from './ui/moving-border';
+
+export default () => {
+    return (
+        <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative mx-auto py-10 md:py-0 ">
+            <Spotlight
+                className="-top-40 left-0 md:left-60 md:-top-20"
+                fill="white"
+            />
+            <div className="p-4 relative z-10 w-full text-center">
+                <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+                    {' '}
+                    Embark on Your Journey: Mastering Web Development
+                </h1>
+                <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+                    "Discover courses tailored to your level and interests. Our
+                    curriculum covers essential concepts and advanced
+                    strategies, empowering you to excel in web development. Join
+                    us to unlock your potential and create captivating online
+                    experiences."
+                </p>
+                <div className="mt-4">
+                    <Link href="/courses">
+                        <Button
+                            borderRadius="2rem"
+                            className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                        >
+                            Explore Courses
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React, { useState } from 'react';
+import { HoveredLink, Menu, MenuItem, ProductItem } from './ui/navbar-menu';
+import { cn } from '@/app/utils/cn';
+import Link from 'next/link';
+
+export default ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+        <div
+            className={cn(
+                'fixed top-10 inset-x-0 max-w-2xl mx-auto z-50',
+                className
+            )}
+        >
+            <Menu setActive={setActive}>
+                <Link href="/">
+                    <MenuItem
+                        active={active}
+                        setActive={setActive}
+                        item="Home"
+                    ></MenuItem>
+                </Link>
+                <MenuItem setActive={setActive} active={active} item="Courses">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/web_developement">
+                            Introduction to Web Development
+                        </HoveredLink>
+                        <HoveredLink href="/html_css">
+                            Advanced HTML and CSS
+                        </HoveredLink>
+                        <HoveredLink href="/javascript">
+                            Introduction to Javascript
+                        </HoveredLink>
+                        <HoveredLink href="/courses">All Courses</HoveredLink>
+                    </div>
+                </MenuItem>
+                <Link href='/contact-Us'>
+                    <MenuItem
+                        setActive={setActive}
+                        active={active}
+                        item="Contact Us"
+                    ></MenuItem>
+                </Link>
+            </Menu>
+        </div>
+    );
+};
