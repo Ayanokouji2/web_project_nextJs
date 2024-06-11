9d31d5ffe6ebe7a05dd0ded00acb6500e49bcfa7
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-06-11 22:59:03 +0530

    Improve footer layout and add grid items

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,39 @@
 export default function Footer() {
     return (
-        <footer className="h-[30rem] grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 py-[10rem] px-12">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+        <footer className="border-t h-[20rem] grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 py-7 px-12">
+            <div className='flex flex-col items-center text-left'>
+                <h2 className='text-xl font-bold text-center text-gray-100'>Customer Care</h2>
+                <p>Contact Us</p>
+                <p>FAQ</p>
+                <p>Returns & Exchange</p>
+                <p>Shipping  Handling</p>
+                <p>Damage or Defected Items</p>
+            </div>
+            <div className='flex flex-col items-center text-left'>
+                <h2 className='text-xl font-bold text-center text-gray-100'>Inside Grace Eleyae</h2>
+                <p>About Us</p>
+                <p>Why Sattin</p>
+                <p>Careers</p>
+                <p>WholeSale</p>
+                <p>GE Magzine</p>
+            </div>
+            <div className='flex flex-col items-center text-left'>
+                <h2 className='text-xl font-bold text-center text-gray-100'>My Account</h2>
+                <p>Sign in/ Register</p>
+                <p>My Wishlist</p>
+                <p>My cart</p>
+                <p>My Payment</p>
+                <p>My Courses</p>
+            </div>
+            <div className='flex flex-col items-center text-left'>
+                <h2 className='text-xl font-bold text-center text-gray-100'>Top Searches</h2>
+                <p>Web Development</p>
+                <p>Backend Development</p>
+                <p>FrontEnd Development</p>
+                <p>AI/ML</p>
+                <p>Database</p>
+            </div>
+           
         </footer>
-    )
+    );
 }
